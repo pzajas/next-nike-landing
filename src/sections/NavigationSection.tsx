1e2e722e5dc2c43dcd4f3926bca27d3eff05e581
pzajas <zajas.piotr@gmail.com>
2025-01-02 22:20:42 +0100

    Create initial hero section.

--- src/sections/NavigationSection.tsx
+++ src/sections/NavigationSection.tsx
@@ -5,11 +5,11 @@ const NavigationSection = () => {
   const links = ['Home', 'About', 'Services', 'Contact']
 
   return (
-    <header className="relative w-full py-8">
-      <nav className="flex justify-between items-center w-full max-w-screen-xl mx-auto px-4">
+    <header className="absolute top-0 left-0 w-full z-10 bg-transparent">
+      <nav className="flex justify-between items-center w-full py-6">
         <Image src="/images/header-logo.svg" alt="Logo" width={129} height={29} />
 
-        <div className="hidden lg:flex space-x-24">
+        <div className="hidden lg:flex space-x-3">
           {links.map((link, index) => (
             <p key={index} className="interactive-link">
               {link}
@@ -17,7 +17,7 @@ const NavigationSection = () => {
           ))}
         </div>
 
-        <p className="hidden lg:flex text-xl cursor-pointer">Sign In</p>
+        <p className="hidden lg:flex text-xl cursor-pointer w-[129px] justify-end items-center">Sign In</p>
 
         <div className="lg:hidden flex items-center">
           <RxHamburgerMenu size={30} />
